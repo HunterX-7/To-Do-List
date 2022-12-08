6a7fef805f3c5af44b6637789dc1a8e7cabf450b
HunterX-7 <fraquin7@gmail.com>
2022-12-08 01:15:10 -0500

    Add the logic for the tasks to display dynamically from the content of the array of objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './style.css';
+
+const toDoTask = [
+  {
+    description: 'Finish Carry Overs',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Go for a 15 min walk',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Finish this week project',
+    completed: false,
+    index: 2,
+  },
+];
+
+const table = document.getElementById('table-body');
+
+const displayList = () => {
+  for (let i = 0; i < toDoTask.length; i += 1) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>
+        <p><span>${toDoTask[i].completed} |</span> ${toDoTask[i].description}</p>
+      </td>
+      <td class="iAlign">${toDoTask[i].index}</td>
+    `;
+    table.appendChild(row);
+  }
+};
+
+window.addEventListener('load', displayList);
