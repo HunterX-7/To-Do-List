d4dbd0ea6b3cf2a0161e129bf5712ebd237e827d
HunterX-7 <fraquin7@gmail.com>
2022-12-15 01:09:43 -0500

    Add an index value that updates to each item in the list

--- src/index.js
+++ src/index.js
@@ -8,29 +8,26 @@ const table = document.getElementById('table-body');
 const form = document.getElementById('toDoForm');
 const addToList = document.getElementById('addToList');
 
-// Form Submit
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  addToDo();
-  displayToDo();
-  localStorage.setItem('todos', JSON.stringify(todos));
-});
-
 // Add to Do
 
 function addToDo() {
   const task = addToList.value;
-  const num = 0;
 
   todos.push({
     description: task,
     completed: false,
-    index: num,
+    index: todos.length,
   });
   addToList.value = '';
-  console.log(todos);
+  console.log(todos.length);
+}
+
+// Update To Do List
+
+function updateToDo() {
+  for (let i = 0; i < todos.length; i += 1) {
+    todos[i].index = i;
+  }
 }
 
 // Display to Do
@@ -52,6 +49,31 @@ function displayToDo() {
   });
 }
 
+function editToDo(todoId) {
+  const row = document.getElementById(todoId);
+  const input = document.getElementById(`edit${todoId}`);
+  row.classList.add('highlight');
+  input.removeAttribute('readonly');
+  input.focus();
+  input.addEventListener('blur', () => {
+    row.classList.remove('highlight');
+    input.setAttribute('readonly', true);
+    todos[todoId].description = input.value;
+    console.log(input.value);
+    displayToDo();
+    localStorage.setItem('todos', JSON.stringify(todos));
+  });
+  console.log(input.value);
+  console.log(todos[todoId].description);
+}
+
+function removeToDo(todoId) {
+  todos = todos.filter((todo, index) => index !== todoId);
+  updateToDo();
+  displayToDo();
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 // Event Listeners
 
 table.addEventListener('click', (e) => {
@@ -67,32 +89,18 @@ table.addEventListener('click', (e) => {
   const { attribute } = target.dataset;
   console.log(todoId, attribute);
 
-  attribute === "edit" && editToDo(todoId);
+  if (attribute === 'edit' && editToDo(todoId));
   if (attribute === 'remove' && removeToDo(todoId));
 });
 
-function editToDo(todoId) {
-  const row = document.getElementById(todoId);
-  const input = document.getElementById(`edit${todoId}`);
-  row.classList.add('highlight');
-  input.removeAttribute('readonly');
-  input.focus();
-  input.addEventListener('blur', (e) => {
-    row.classList.remove('highlight')
-    input.setAttribute('readonly', true);
-    todos[todoId].description = input.value;
-    console.log(input.value)
-    displayToDo();
-    localStorage.setItem('todos', JSON.stringify(todos));
-  })
-  console.log(input.value)
-  console.log(todos[todoId].description)
-}
+// Form Submit
 
-function removeToDo(todoId) {
-  todos = todos.filter((todo, index) => index !== todoId);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addToDo();
   displayToDo();
   localStorage.setItem('todos', JSON.stringify(todos));
-}
+});
 
 window.addEventListener('load', displayToDo);
